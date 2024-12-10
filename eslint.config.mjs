31b49bda210fe47e707bf9ddb2a8ab29a5ece50a
cieloazul310 <cieloazul310@gmail.com>
2024-12-10 20:27:14 +0900

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import importPlugin from "eslint-plugin-import";
 import eslintPluginAstro from "eslint-plugin-astro";
+import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
 import astroParser from "astro-eslint-parser";
 import typescriptEslintParser from "@typescript-eslint/parser";
 import eslintConfigPrettier from "eslint-config-prettier";
@@ -12,7 +13,12 @@ export default [
   pluginJs.configs.recommended,
   importPlugin.flatConfigs.recommended,
   ...tseslint.configs.recommended,
-  ...eslintPluginAstro.configs.recommended,
+  {
+    plugins: {
+      "jsx-a11y": jsxA11yPlugin,
+    },
+  },
+  ...eslintPluginAstro.configs["jsx-a11y-recommended"],
   {
     languageOptions: {
       ecmaVersion: 2022,
